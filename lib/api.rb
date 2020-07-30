6c7f812bd1cc391b81ddff0feb300c4bf18df90a
carterp <cparrett94@hotmail.com>
2020-07-30 17:43:46 -0500

    finalized api class into working condition

--- lib/api.rb
+++ lib/api.rb
@@ -1,22 +1,23 @@
-require 'pry'
 require 'httparty'
-
+require 'pry'
 class API
 
 
-    # key = "c3a09436c36df61cfcf77bb566836dbb"
+    @@key = "c3a09436c36df61cfcf77bb566836dbb"
 
     def self.get_movies(movie_name)
-        # key = "c3a09436c36df61cfcf77bb566836dbb"
-        response = HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=c3a09436c36df61cfcf77bb566836dbb&query=#{movie_name}")
+        
+        response = HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=#{@@key}&query=#{movie_name}")
         movie_hash = {}
         movie_hash[:overview] = response["results"][0]["overview"]
         movie_hash[:title] = response["results"][0]["title"]
         movie_hash[:score] = response["results"][0]["vote_average"]
-        binding.pry
+        movie_hash
+        # binding.pry
     end
-    
+
 end
 
-new = API.get_movies("batman")
-new
+# new = API.get_movies("batman")
+# puts new
+
