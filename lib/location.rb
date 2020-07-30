eaeb3b0a84706c71bd0a47f0cea654975f22ef66
carterp <cparrett94@hotmail.com>
2020-07-30 17:56:40 -0500

    created initialize method inside movie class

--- lib/location.rb
+++ lib/location.rb
@@ -2,7 +2,12 @@ class Movie
 
     attr_accessor :title, :overview, :score
 
-   
+    def initialize(movie)
+        movie.each {|key, value| self.send({"#{key}="}, value)
+        @@all << self        
+    end
+
+    
 
 
 end
