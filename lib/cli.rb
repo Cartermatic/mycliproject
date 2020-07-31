4108c251710134c82177d7de362a9112c0a85c7a
carterp <cparrett94@hotmail.com>
2020-07-31 12:16:42 -0500

    added welcome greeting to cli class

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,4 +1,34 @@
+require 'pry'
+
 class CLI
+
+
+
+
+# def movie_time_image
+
+# puts " "
+# puts '   ___  ___           _        _____ _                 '
+# puts '   |  \/  |          (_)      |_   _(_)                '
+# puts '   | .  . | _____   ___  ___    | |  _ _ __ ___   ___  '
+# puts '   | |\/| |/ _ \ \ / / |/ _ \   | | | | '_ ` _ \ / _ \ '
+# puts '   | |  | | (_) \ V /| |  __/   | | | | | | | | |  __/ '
+# puts '   \_|  |_/\___/ \_/ |_|\___|   \_/ |_|_| |_| |_|\___| '
+# puts " "                                                       
+# end
+
+# movie_time_image
+
+    def welcome_greeting
+        puts "Hey there! Thanks for using my film searching program, Movie Time!"
+        puts " "
+        puts "Just search for a film and I will do my best to give you some quick information about it!"
+        puts " "
+    end
+
+
+
+
 
 
 end
